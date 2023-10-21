457256f1a37d75239391f360f80e98dce8bc6960
Facundo Martin Gordillo <me@facundogordillo.com>
2023-10-21 05:31:13 +0200

    feat: i like this config more

--- nvim/init.lua
+++ nvim/init.lua
@@ -42,7 +42,6 @@ require('lazy').setup({
   'tpope/vim-sleuth',
 
   'Exafunction/codeium.vim',
-  'airblade/vim-gitgutter',
   'wakatime/vim-wakatime',
 
   -- Hide keys from prying eyes
@@ -118,10 +117,10 @@ require('lazy').setup({
         changedelete = { text = '~' },
       },
       on_attach = function(bufnr)
-        vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
+        vim.keymap.set('n', '<leader>g[', require('gitsigns').prev_hunk,
           { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
-        vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
-        vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
+        vim.keymap.set('n', '<leader>g]', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
+        vim.keymap.set('n', '<leader>gp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[G]it [P]review Hunk' })
       end,
     },
   },
