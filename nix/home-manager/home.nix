7213ba53632d0fc55664a06875d55f4df2735599
Facundo Martin Gordillo <me@facundogordillo.com>
2024-02-27 22:09:47 +0100

    feat: Added auto upgrade + linting

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -3,6 +3,10 @@
   pkgs,
   ...
 }: {
+  services.home-manager.autoUpgrade = {
+    enable = true;
+    frequency = "weekly";
+  };
   nixpkgs.config.allowUnfree = true;
 
   nixpkgs.config.permittedInsecurePackages = [
